537831cbf62656905a29913f7d3b589c8ac42842
ribeirocaros87 <cezar.cr@gmail.com>
2022-02-01 11:38:46 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/carlosribeiro/tiptime/ExampleInstrumentedTest.kt
@@ -0,0 +1,39 @@
+package com.carlosribeiro.tiptime
+
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun calculate_20_percent_tip() {
+     onView(withId(R.id.cost)).perform(typeText("50.00"))
+        closeSoftKeyboard()
+     onView(withId(R.id.calc_button)).perform(click())
+     onView(withId(R.id.tip_result))
+         .check(matches(withText(containsString("$10.00"))))
+ }
+}
---
+++ app/src/main/java/com/carlosribeiro/tiptime/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.carlosribeiro.tiptime
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.carlosribeiro.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cost.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+        binding.calcButton.setOnClickListener{ calculateTip()}
+    }
+
+    private fun calculateTip(){
+
+        val stringInTextField = binding.cost.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option20pc -> 0.20
+            R.id.option18pc -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if(binding.roundSwitch.isChecked){
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hides the keyboard
+            val inputMethodManager =
+               getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
